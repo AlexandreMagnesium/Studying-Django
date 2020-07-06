0a07925b2fb2db7b6b3e7dfaa196cc1235d6c333
AlexandreMagnesium <49409332+alexandremg999@users.noreply.github.com>
2020-07-06 23:55:27 +0300

    creating models

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField('Назва', max_length=50)
+    task = models.TextField('Опис')
+
+    def __str__(self):
+        return self.title
