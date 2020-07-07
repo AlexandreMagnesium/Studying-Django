cae3a025f1284471c5ba3ea4f7d1e2366417015a
AlexandreMagnesium <49409332+alexandremg999@users.noreply.github.com>
2020-07-07 22:47:19 +0300

    adding data to the database

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.order_by('-id')
     return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
 
 def aboutUs(request):
     return render(request, 'main/about-us.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма була неправильною'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
